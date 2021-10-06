e5edbc9eced0df42e40d5ba86110cb6d43087a5c
cybervirus997 <soumyaranjanpurohit46@gmail.com>
2021-10-06 22:06:36 +0530

    base production is done

---
+++ src/data/headerData.js
@@ -0,0 +1,9 @@
+import resume from '../assets/pdf/resume.pdf'
+
+export const headerData = {
+    name: 'Soumya Ranjan Purohit',
+    title: "Web Developer",
+    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+    image: 'https://user-images.githubusercontent.com/72969348/136245630-a6be198c-3aeb-49d3-bb4e-2fb18b771e52.jpg',
+    resumePdf: resume
+}
