82817bf218fb03f5f1fbd81accabd322bd1394d2
cybervirus997 <soumyaranjanpurohit46@gmail.com>
2021-10-28 21:56:40 +0530

    icon fixed

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import React,{ useContext} from 'react';
+import React,{ useContext,useState} from 'react';
 import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -12,6 +12,9 @@ import SingleProject from './SingleProject/SingleProject';
 function Projects() {
 
     const { theme } = useContext(ThemeContext);
+
+    const [next, setNext] = useState(0);
+    const [direction,setDirection] = useState(false);
 
     
     const useStyles = makeStyles(() => ({
@@ -43,6 +46,21 @@ function Projects() {
 
     const classes = useStyles();
 
+    window.addEventListener('resize', () => { window.innerWidth < 993 ? setDirection(true) :  setDirection(false)})
+
+    const handlePrev = () => {
+        if (next !== 0)
+        {
+            setNext(next-1)
+        }
+    }
+    const handleNext= () => {
+        if (next !== 2)
+        {
+            setNext(next+1)
+        }
+    }
+
     return (
         <>
             {projectsData.length > 0 && (
@@ -52,7 +70,16 @@ function Projects() {
                     </div>
                     <div className="projects--body">
                         <div className="projects--bodyContainer">
-                            {projectsData.slice(0, 3).map(project => (
+                            <button style={{ borderRadius: "50%", width: "39px", border: "none", height: "39px", outline: "none" }} onClick={handlePrev}>
+
+                                { direction ? <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z" />
+                                </svg> : <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+                                </svg> }
+                            
+                            </button>
+                            {projectsData.slice(0+next, 3+next).map(project => (
                                 <SingleProject
                                     theme={theme}
                                     key={project.id}
@@ -66,6 +93,15 @@ function Projects() {
                                     image={project.image}
                                 />
                             ))}
+                            <button style={{ borderRadius: "50%", width: "39px", border: "none", height: "39px", outline: "none" }} onClick={handleNext}>
+
+                                { direction ? <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z" />
+                                </svg> : <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                                </svg> }
+
+                            </button>
                         </div> 
 
                         {/* {projectsData.length > 3 && (
