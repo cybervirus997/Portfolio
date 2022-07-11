b0e8f1f0a2094d6f2fa9cd879a82c197b2988907
Soumyaranjan Purohit <72969348+cybervirus997@users.noreply.github.com>
2022-07-11 20:54:27 +0530

    expperice added

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,12 +1,18 @@
 export const experienceData = [
     {
         id: 1,
+        company: 'Trinkerr',
+        jobtitle: 'Software Engg.',
+        startYear: '2021',
+    },
+    {
+        id: 2,
         company: 'Electronics Coporation of India Limited,Hyderabad',
         jobtitle: 'Research Trainee',
         startYear: '2021',
     },
     {
-        id: 2,
+        id: 3,
         company: 'UltaTech,Jharsuguda',
         jobtitle: 'Trainee',
         startYear: '2021',
