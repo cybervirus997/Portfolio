5433768eb6d2e85030a0cef1bc3ff4d4f3e0daa4
cybervirus997 <soumyaranjanpurohit46@gmail.com>
2022-10-23 13:50:29 +0530

    profile page updated and new dev started

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
-import resume from '../assets/pdf/resume.pdf'
+import resume from "../assets/pdf/resume.pdf";
 
 export const headerData = {
-    name: 'Soumya Ranjan Purohit',
-    title: "Hello! I'am",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://user-images.githubusercontent.com/72969348/137774931-0a86984e-cd98-417a-b004-14434dd06b3a.jpg',
-    resumePdf: resume
-}
+  name: "Soumya Ranjan Purohit",
+  title: "Hello! I'am",
+  image:
+    "https://user-images.githubusercontent.com/72969348/197381176-5c56ff1b-61e5-46c2-a593-e538e9569c99.jpg",
+  resumePdf: resume,
+};
