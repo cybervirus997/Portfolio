2fb36e295c1b231df83ac054993093f92fa7334e
Soumyaranjan Purohit <72969348+cybervirus997@users.noreply.github.com>
2024-10-24 19:02:01 +0530

    updation

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -30,14 +30,3 @@ export const achievementData = {
 }
 
 
-// Do not remove any fields.
-// Leave it blank instead as shown below.
-
-/*
-
-export const achievementData = {
-    bio : "",
-    achievements : []
-}
-
-*/
--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,18 +1,30 @@
 export const experienceData = [
     {
         id: 1,
-        company: `Trinkerr - India's First Social Trading Platform`,
-        jobtitle: 'Software Engineer',
-        startYear: 'Dec 2021 - Present',
+        company: `TIQS`,
+        jobtitle: 'SDE-1',
+        startYear: 'Jan 2024 - Present',
     },
     {
         id: 2,
+        company: `Trinkerr - India's First Social Trading Platform`,
+        jobtitle: 'Software Engineer',
+        startYear: 'Aug 2022 - Jan 2024',
+    },
+    {
+        id: 3,
+        company: `Trinkerr - India's First Social Trading Platform`,
+        jobtitle: 'Associate Software Engineer',
+        startYear: 'Dec 2021 - Aug 2022',
+    },
+    {
+        id: 4,
         company: 'Electronics Coporation of India Limited,Hyderabad',
         jobtitle: 'Research Trainee',
         startYear: 'Feb 2021 - Apr 2021',
     },
     {
-        id: 3,
+        id: 5,
         company: 'UltaTech,Jharsuguda',
         jobtitle: 'Trainee',
         startYear: 'Jan 2021 - Feb 2021',
@@ -20,12 +32,3 @@ export const experienceData = [
 ]
 
 
-//schema for entering the data
-
-    // {
-    //     id: 3,
-    //     company: '',
-    //     jobtitle: '',
-    //     startYear: '',
-    //     endYear: ''
-    // },
